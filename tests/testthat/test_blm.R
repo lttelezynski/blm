9a978cb89bc9862f4b84f53c8a97eefb61b383b3
lttelezynski <lttelezynski@gmail.com>
2016-12-20 11:56:20 +0100

    Some initial test.

--- tests/testthat/test_blm.R
+++ tests/testthat/test_blm.R
@@ -2,4 +2,16 @@ context("blm")
 
 test_that("we can fit blm objects", {
   # Put some initial tests here.
+  x <- stats::rnorm(100, 10, 5)
+  z<- stats::rnorm(100, 5, 5)
+  a <- 1
+  b <- 2
+  c<-3
+  y <- a+b*x
+
+  fit <- blm(y~x,1,1)
+  cof<-coefficients(fit)
+  names(cof)<-NULL
+  expect_equal(cof[1], a, tolerance=0.1)
+  expect_equal(cof[2], b, tolerance=0.1)
 })
